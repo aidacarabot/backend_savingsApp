037005ef9483b4b49600e02f7dbc583875b9d72e
Aida Carabot <aidacarabot@146.113.177.224.hwccustomers.com>
2025-07-23 15:07:09 -0400

    Transactions files updated

--- src/api/controllers/transaction.js
+++ src/api/controllers/transaction.js
@@ -1,34 +1,28 @@
-const mongoose = require('mongoose');
-const Transaction = require('../../api/models/transaction');
-const User = require('../models/user');
+const Transaction = require('../../api/models/transaction')
+const User = require('../models/user')
 
 //! GET ALL TRANSACTIONS
 const getAllTransactions = async (req, res) => {
   try {
-    const transactions = await Transaction.find(); // Obtener todas las transacciones
-    return res.status(200).json(transactions); // Enviar las transacciones como respuesta
+    const transactions = await Transaction.find() // Obtener todas las transacciones
+    return res.status(200).json(transactions) // Enviar las transacciones como respuesta
   } catch (error) {
-    return res.status(500).json({ message: "Error fetching transactions:", error: error.message });
+    return res
+      .status(500)
+      .json({ message: 'Error fetching transactions:', error: error.message })
   }
 }
 
 //! ADD NEW TRANSACTION
 const addTransaction = async (req, res) => {
-  const { type, name, amount, date, category, user } = req.body;
+  const { type, name, amount, date, category, user } = req.body
 
   // Validar que si el tipo es "Expense", se debe especificar la categoría
   if (type === 'Expense' && !category) {
-    return res.status(400).json("Category is required for expenses");
+    return res.status(400).json('Category is required for expenses')
   }
 
   try {
-    // Validar que el ID del usuario es válido
-    if (!mongoose.Types.ObjectId.isValid(user)) {
-      return res.status(400).json("Invalid user ID");
-    }
-
-    console.log("User ID:", user);  // Depurar el ID del usuario
-
     // Crear una nueva transacción
     const newTransaction = new Transaction({
       type,
@@ -37,84 +31,82 @@ const addTransaction = async (req, res) => {
       date,
       category: type === 'Expense' ? category : undefined, // Si no es "Expense", no asignamos category
       user
-    });
+    })
 
     // Guardar la transacción en la base de datos
-    const savedTransaction = await newTransaction.save();
+    const savedTransaction = await newTransaction.save()
 
-    // Verificar si el usuario existe
-    const userToUpdate = await User.findById(new mongoose.Types.ObjectId(user));
-    console.log('Result of User.findById:', userToUpdate); // Add this line to inspect the result
-    if (!userToUpdate) {
-      return res.status(404).json("User not found");
-    }
+    // Añadir la transacción al array de transacciones del usuario
+    await User.findByIdAndUpdate(
+      user,
+      { $push: { transactions: savedTransaction._id } }
+    );
 
-    // Actualizar el campo `transactions` del usuario con el ID de la nueva transacción
-    const updatedUser = await User.findByIdAndUpdate(user, {
-      $push: { transactions: savedTransaction._id }  // Agregar el ID de la nueva transacción al array `transactions`
-    }, { new: true });  // Asegúrate de que se retorne el usuario actualizado
-
-    console.log('Updated User:', updatedUser);  // Verificar el usuario actualizado
-
-    res.status(201).json(savedTransaction); // Enviar la transacción recién creada
+    res.status(201).json(savedTransaction) // Enviar la transacción recién creada
   } catch (error) {
-    res.status(500).json({ message: "Error adding transaction", error: error.message });
+    res
+      .status(500)
+      .json({ message: 'Error adding transaction', error: error.message })
   }
-};
+}
 
 //! DELETE TRANSACTION
 const deleteTransaction = async (req, res) => {
-  const { id } = req.params; //obtener el id de transaction
+  const { id } = req.params //obtener el id de transaction
 
   try {
-    const deletedTransaction = await Transaction.findByIdAndDelete(id); //eliminamos la transaccion y si no se encuentra...
+    const deletedTransaction = await Transaction.findByIdAndDelete(id) //eliminamos la transaccion y si no se encuentra...
     if (!deletedTransaction) {
-      return res.status(404).json("Transaction not found");
+      return res.status(404).json('Transaction not found')
     }
 
-    res.status(200).json("Transaction deleted successfully");
+    res.status(200).json('Transaction deleted successfully')
   } catch (error) {
-    res.status(500).json({ message: "Error deleting transaction:", error: error.message });
+    res
+      .status(500)
+      .json({ message: 'Error deleting transaction:', error: error.message })
   }
 }
 
 //! EDIT TRANSACTION
 const editTransaction = async (req, res) => {
-  const { id } = req.params;
-  const { type, name, amount, date, category } = req.body;
+  const { id } = req.params
+  const { type, name, amount, date, category } = req.body
 
   try {
-    const transaction = await Transaction.findById(id); //buscamos transaction por id
+    const transaction = await Transaction.findById(id) //buscamos transaction por id
     if (!transaction) {
-      return res.status(404).json("Transaction not found");
+      return res.status(404).json('Transaction not found')
     }
 
     //? Si el tipo es "Expense", asignamos la categoría
     if (type === 'Expense') {
-      transaction.category = category || transaction.category;
+      transaction.category = category || transaction.category
     } else {
       //? Si el tipo es "Income", eliminamos la categoría
-      transaction.category = undefined;
+      transaction.category = undefined
     }
 
     //? Actualizamos el resto de campos
     //property = newValue || existingValue;
-    transaction.type = type || transaction.type;
-    transaction.name = name || transaction.name;
-    transaction.amount = amount || transaction.amount;
-    transaction.date = date || transaction.date;
+    transaction.type = type || transaction.type
+    transaction.name = name || transaction.name
+    transaction.amount = amount || transaction.amount
+    transaction.date = date || transaction.date
 
     //? Guardamos la transacción actualizada
-    const updatedTransaction = await transaction.save(); // guardamos los cambios en la DB
-    res.status(200).json(updatedTransaction); // Devolvemos la transacción actualizada
+    const updatedTransaction = await transaction.save() // guardamos los cambios en la DB
+    res.status(200).json(updatedTransaction) // Devolvemos la transacción actualizada
   } catch (error) {
-    res.status(500).json({ message: "Error updating transaction:", error: error.message });
+    res
+      .status(500)
+      .json({ message: 'Error updating transaction:', error: error.message })
   }
-};
+}
 
 module.exports = {
   getAllTransactions,
   addTransaction,
   deleteTransaction,
   editTransaction
-};
+}
